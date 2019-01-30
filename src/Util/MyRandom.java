c2812d6a2cd70ec5db80d3531d13a57480f51bfb
gaborKriger <regirck@gmail.com>
2019-01-30 11:22:28 +0100

    MyRandom evenWithChance();

--- src/Util/MyRandom.java
+++ src/Util/MyRandom.java
@@ -4,8 +4,12 @@ import java.util.Random;
 
 public class MyRandom {
 
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
+    public MyRandom() {
+    }
 
-
+    public static boolean eventWithChance(int chance) {
+        return RANDOM.nextInt(100) < chance;
+    }
 }
